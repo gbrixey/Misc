23f33ab9fb3b9709bac0e96dc97847efebf9cb10
Glen Brixey <glenb@zillow.com>
2019-12-02 12:53:52 -0500

    Update playlist

--- playlist_monitor.py
+++ playlist_monitor.py
@@ -12,7 +12,8 @@ TEAM_ID = os.environ['PM_TEAM_ID']
 DATABASE = 'ambient_essentials.db'
 
 def jwt_token():
-    # Using the default p8 file name
+    '''Creates a JWT token from the p8 file stored in the current directory. This function expects
+    the p8 filename to be in the default format AuthKey_TEAM.p8 where TEAM is the team ID provided by Apple.'''
     with open('AuthKey_{0}.p8'.format(KEY_ID)) as key_file:
         key = key_file.read()
     iat = datetime.datetime.utcnow()
@@ -24,6 +25,7 @@ def jwt_token():
     return token
 
 def ensure_table(db):
+    '''Creates the playlist tracks table in the given database if it doesn't already exist.'''
     sql_create_table = '''
         create table if not exists tracks (
             track_id integer primary key,
@@ -36,6 +38,9 @@ def ensure_table(db):
     db.cursor().execute(sql_create_table)
 
 def update_entry(db, track_id, artist, name, current_date):
+    '''Updates the database with the given parameters. If a row with a matching track_id already exists
+    in the database, this function updates the artist, name, and last_seen columns for that row.
+    Otherwise it inserts a new row.'''
     sql_upsert = '''
         insert into tracks (track_id, artist, name, first_seen, last_seen) values (?, ?, ?, ?, ?)
             on conflict(track_id) do update set
@@ -45,7 +50,17 @@ def update_entry(db, track_id, artist, name, current_date):
     '''
     db.cursor().execute(sql_upsert, (track_id, artist, name, current_date, current_date))
 
-def fetch_playlist():
+def get_playlist_tracks_from_database():
+    '''Gets all tracks from the database.'''
+    db = sqlite3.connect(DATABASE)
+    ensure_table(db)
+    sql_fetch = '''select * from tracks order by track_id desc'''
+    tracks = db.cursor().execute(sql_fetch).fetchall()
+    db.close()
+    return tracks
+
+def fetch_playlist_from_web():
+    '''Fetches the Ambient Essentials playlist from the internet and returns the result in JSON format.'''
     token = jwt_token()
     headers = {'Authorization': 'Bearer {0}'.format(token)}
     response = requests.get(URL, headers = headers)
@@ -53,9 +68,10 @@ def fetch_playlist():
     return playlist
 
 def update_tracks():
+    '''Fetches the Ambient Essentials playlist from the internet and updates the tracks in the database.'''
     db = sqlite3.connect(DATABASE)
     ensure_table(db)
-    playlist = fetch_playlist()
+    playlist = fetch_playlist_from_web()
     last_modified = playlist['attributes']['lastModifiedDate']
     tracks = playlist['relationships']['tracks']['data']
     for track in tracks:
