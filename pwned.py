407a2c4b62469484fac9bf1339a68d3f99d00cd3
Glen Brixey <glen.brixey@gmail.com>
2025-03-07 17:03:40 -0600

    Update HIBP script
    - Use pwinput instead of getpass
    - Add a loop to make it easier to check multiple passwords
    - Use f-strings

--- pwned.py
+++ pwned.py
@@ -1,30 +1,38 @@
-#!/usr/bin/env python
-import getpass
+#!/usr/bin/env python3
 import hashlib
+import pwinput
 import requests
 
-def pwned():
+def pwned_main():
+    '''Repeatedly asks the user to enter a password, 
+    and calls the pwned_check_password function for each password.'''
+    while True:
+        print('Enter a password to check, or Q to quit.')
+        password = pwinput.pwinput("Password: ")
+        if password.lower() == 'q' or len(password) == 0:
+            return
+        pwned_check_password(password)
+
+def pwned_check_password(password):
     '''Checks if the given password has been exposed in major data breaches
     using the haveibeenpwned.com API. The password itself is not uploaded
     to the Internet.'''
-    print('Enter a password to check.')
-    password = getpass.getpass()
     encoded = password.encode('utf-8')
     sha = hashlib.sha1(encoded).hexdigest()
     prefix = sha[:5]
-    url = 'https://api.pwnedpasswords.com/range/{0}'.format(prefix)
+    url = f'https://api.pwnedpasswords.com/range/{prefix}'
     response = requests.get(url)
     if response.status_code != 200:
-        print('Request failed with code {0}'.format(response.status_code))
+        print(f'Request failed with code {response.status_code}')
         return
     results = response.content.decode('utf-8').split()
     for result in results:
         components = result.split(':')
         suffix = components[0].lower()
         if sha == prefix + suffix:
-            print('This password was exposed in {0} data breaches'.format(components[1]))
+            print(f'This password was exposed in {components[1]} data breaches.\n')
             return
-    print('Good news! This password was not found in major data breaches.')
+    print('Good news! This password was not found in major data breaches.\n')
 
 if __name__ == '__main__':
-    pwned()
+    pwned_main()
